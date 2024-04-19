bea4868679fef5c59d3ee0d871a0a62347fabeba
matiasbianco8 <67788368+matiasbianco8@users.noreply.github.com>
2024-04-19 14:38:56 -0300

    Update brain.cs

--- Assets/brain.cs
+++ Assets/brain.cs
@@ -7,7 +7,7 @@ public class brain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Hola
+        // Hola. Como estas?
     }
 
     // Update is called once per frame
